2cbbc35d0aa9910aef2cee041fae8f387db5e12e
Shanti Chellaram <shanti@shanti.im>
2019-04-03 18:12:31 -0400

    Add some argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,59 @@
 
+use std::env;
 use std::io;
+use std::io::ErrorKind;
+
+fn lol() -> u8 {
+    'o' as u8
+}
+
+fn print_usage() {
+    eprintln!("usage: terribledb <node_name>");
+}
+
+fn parse_args(args: Vec<String>) -> io::Result<String> {
+    match args.len() {
+        2 => {
+            Ok(args[1].clone())
+        },
+        _ => {
+            eprintln!("error: incorrect number of arguments");
+            print_usage();
+            Err(io::Error::new(ErrorKind::InvalidInput, "incorrect number of arguments"))
+        }
+    }
+}
+
+struct TerribleConfig {
+    name: String
+}
 
 fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let result = parse_args(args)?;
+
+    main_loop(|input: &str| -> bool {input.trim() == "stahp"})
+}
+
+fn main_loop<F> (mut break_check: F) -> io::Result<()>
+where F: FnMut(&str) -> bool {
     loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        if input.trim() == "stahp" {
+        println!("You typed: {}", input.trim());
+        if break_check(&input) {
             break;
         }
-        println!("You typed: {}", input.trim());
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(lol(), 'o' as u8);
+    }
 }
